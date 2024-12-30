113bb44437d7c7679c1a09f6bd47393245ad2a3f
SharathGowda m k <sharath@SharathGowdas-MacBook-Pro.local>
2024-12-30 21:13:45 +0530

    changed-class-name and started leraning java and git

---
+++ src/JavaProgPrep/BasicsUnderStanding.java
@@ -0,0 +1,11 @@
+package JavaProgPrep;
+
+public class BasicsUnderStanding {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java world");
+
+    }
+
+
+}
--- src/JavaProgPrep/CountCharacter.java
+++
@@ -1,5 +0,0 @@
-package JavaProgPrep;
-
-public class CountCharacter {
-
-}
