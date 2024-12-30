42dcb378cad28b455f071aed0f96f8a41c18890a
sharathabd89 <sharath@SharathGowdas-MacBook-Pro.local>
2024-12-30 21:25:13 +0530

    added another line code

--- src/JavaProgPrep/BasicsUnderStanding.java
+++ src/JavaProgPrep/BasicsUnderStanding.java
@@ -4,7 +4,7 @@ public class BasicsUnderStanding {
     public static void main(String[] args)
     {
         System.out.println("Hello Java world");
-
+System.out.println("hello all the best");
     }
 
 
