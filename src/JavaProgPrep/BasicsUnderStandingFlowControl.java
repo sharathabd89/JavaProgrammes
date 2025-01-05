d50998917f941c42774fc162de79195854c38d76
sharathabd89 <sharathabd89@gmail.com>
2025-01-05 10:48:29 +0530

    Learning the java cntrolflow

---
+++ src/JavaProgPrep/BasicsUnderStandingFlowControl.java
@@ -0,0 +1,18 @@
+package JavaProgPrep;
+
+public class BasicsUnderStandingFlowControl {
+    public static void main(String[] args)
+    {
+        //understanding the control flow
+        int a=10;
+        int b=23;
+        if(a>=b)
+        {
+            System.out.println("Good mrng");
+        }
+        System.out.println("Good Afternoon");
+
+    }
+
+
+}
