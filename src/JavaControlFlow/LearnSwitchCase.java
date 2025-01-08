85cc2d23081fa6289f672deeffbdb43b99863424
sharathabd89 <sharathabd89@gmail.com>
2025-01-08 13:00:21 +0530

    Switchcase-1

---
+++ src/JavaControlFlow/LearnSwitchCase.java
@@ -0,0 +1,50 @@
+package JavaControlFlow;
+
+public class LearnSwitchCase {
+    public static void main(String[] args)
+    {
+        int a=11;
+        switch (a) {
+            case 1:
+                System.out.println("JANUARY MONTH");
+                break;
+
+            case 2:
+                System.out.println("FEBRUARY MONTH");
+                break;
+
+            case 3:
+                System.out.println("MARCH MONTH");
+                break;
+            case 4:
+                System.out.println("APRIL MONTH");
+                break;
+            case 5:
+                System.out.println("MAY MONTH");
+                break;
+            case 6:
+                System.out.println("JUN MONTH");
+                break;
+            case 7:
+                System.out.println("JULY MONTH");
+                break;
+            case 8:
+                System.out.println("AUGUST MONTH");
+                break;
+            case 9:
+                System.out.println("SEP MONTH");
+                break;
+            case 10:
+                System.out.println("OCTOBER MONTH");
+                break;
+            case 11:
+                System.out.println("NOVEMBER MONTH");
+                break;
+            case 12:
+                System.out.println("DECEMBER MONTH");
+                break;
+            default:
+                System.out.println("Not a valid month for given value");
+        }}}
+
+
